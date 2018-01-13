6249f358f8c449647b434ddc0dbfcd3cd2460346
lapinell <lapinell@gmail.com>
2018-01-13 09:54:58 -0600

    created products html and js files

---
+++ js/products.js
@@ -0,0 +1,56 @@
+// Object properties: name, description, price, and URL for product image
+var products = [
+    {
+        name: 'app name 0',
+        description: 'descriptive text',
+        price: 10,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 1',
+        description: 'descriptive text',
+        price: 100,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 2',
+        description: 'descriptive text',
+        price: 200,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 3',
+        description: 'descriptive text',
+        price: 300,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 4',
+        description: 'descriptive text',
+        price: 400,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 5',
+        description: 'descriptive text',
+        price: 500,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 6',
+        description: 'descriptive text',
+        price: 600,
+        imgURL: 'url'
+    },
+    {
+        name: 'app name 7',
+        description: 'descriptive text',
+        price: 700,
+        imgURL: 'url'
+    },
+];
+
+console.log(
+    products[0]
+);
+

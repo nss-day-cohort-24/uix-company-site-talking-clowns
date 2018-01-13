c56e12b785bc6fdb1256e65e4c5d9f86081dab3d
lapinell <lapinell@gmail.com>
2018-01-13 13:39:54 -0600

    added image urls to product objects

--- js/products.js
+++ js/products.js
@@ -4,53 +4,50 @@ var products = [
         name: 'RubyRoute',
         description: ' Remove the worry from your child traveling alone with a tracking app that includes checkpoints, mini-games, and notifications for when your child arrives at their destination.',
         price: '$4.00',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/ruby.png'
     },
     {
         name: 'KeepUp',
         description: 'A simplified calendar built for parents to keep track of their child\'s wild schedule of daily events.',
         price: 'free (in-app purchases)',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/calendar.png'
     },
     {
         name: 'Carriage',
         description: 'Be sure your child is in caring hands while traveling to and from home with our trusted group of drivers.',
         price: 'free',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/carriage.png'
     },
     {
         name: 'Liength',
         description: 'Practice honesty with your child with our lie detecting app that can discreetly analyze if your child is telling true or a fib.',
         price: '$10.00',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/lyingth.png'
     },
     {
         name: 'All Gowned Up',
         description: 'Open your child\'s creative side with a simple yet lively dress designer suitable for any and all levels of imagination.',
         price: 'free (in-app purchases)',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/gown.png'
     },
     {
         name: 'PrincePoint',
         description: 'Find the Prince Charming that fits your fantasy perfectly.',
         price: 'free (in-app purchases)',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/princer.png'
     },
     {
         name: 'Candy Cottage Cookbook',
         description: 'Cookbook app of famous recipes from other stories/fables. Make cooking fun with re-creations of favorite recipes from your child\'s favorite tales and stories.',
         price: '$6.00',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/food.png'
     },
     {
         name: 'Stronk Fitness',
         description: 'Keep you and your child healthy by monitoring things such as their exercise and nutrition. Keep them involved by staying healthy with them and sharing your progress with each other along the way.',
         price: 'free (in-app purchases)',
-        imgURL: 'url'
+        imgURL: '../images/appIcons/workout.png'
     }
 ];
 
-console.log(
-    products[0]
-);
 
